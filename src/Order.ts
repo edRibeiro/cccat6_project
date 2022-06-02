baeca4fe4a07f1c4137bedd5403c40b418e3fe4a
edRibeiro <edersonribeiro.ita@gmail.com>
2022-06-02 15:56:40 -0300

    Deve criar um pedido com 3 items

--- src/Order.ts
+++ src/Order.ts
@@ -1,9 +1,25 @@
 import Cpf from "./Cpf";
-
+import Item from "./Item";
+import OrderItem from "./OrderItem";
 
 export default class Order {
-  cpf: Cpf
+  cpf: Cpf;
+  orderItems: OrderItem[];
+
   constructor(cpf: string) {
     this.cpf = new Cpf(cpf);
+    this.orderItems = [];
+  }
+
+  addItem(item: Item, quantity: number) {
+    this.orderItems.push(new OrderItem(item.idItem, item.price, quantity));
+  }
+
+  getTotal() {
+    const total = this.orderItems.reduce((total, orderItem) => {
+      total += orderItem.getTotal();
+      return total;
+    }, 0);
+    return total;
   }
 }
--- test/Order.test.ts
+++ test/Order.test.ts
@@ -1,5 +1,16 @@
+import { isTemplateExpression } from "typescript";
+import Item from "../src/Item";
 import Order from "../src/Order";
 
 test("Não deve criar um pedido com CPF inválido", function () {
     expect(() => new Order("111.111.111-11")).toThrow(new Error("CPF Inválido"));
+});
+
+test("Deve criar um pedido com 3 items (com descrição, preço e quantidade)", function () {
+    const order = new Order("935.411.347-80");
+    order.addItem(new Item(1, "Gitarra", 1000), 1);
+    order.addItem(new Item(2, "Amplificador", 5000), 1);
+    order.addItem(new Item(3, "Cabo", 30), 3);
+    const total = order.getTotal();
+    expect(total).toBe(6090);
 });
